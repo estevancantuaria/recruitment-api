816002443c78a2273a0ab5a7b39d481cbf692dd4
Estevan <estevancantuaria23@gmail.com>
2025-01-07 21:15:33 -0300

    config: Creating db connection handler with sqlite and tests

---
+++ src/models/sqlite/connections_test.py
@@ -0,0 +1,12 @@
+import pytest
+from sqlalchemy.engine import Engine
+from .connections import db_connection_handler
+
+def test_db_connection_handler():
+    assert db_connection_handler.get_engine() is None
+    
+    db_connection_handler.connect_to_db()
+    db_engine = db_connection_handler.get_engine()
+    
+    assert db_engine is not None
+    assert isinstance(db_engine, Engine)
