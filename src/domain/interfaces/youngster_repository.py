dbcf4cc66273a66aab36e6df62b540d76843cafd
Estevan <estevancantuaria23@gmail.com>
2025-01-25 10:29:23 -0300

    refactor: Change IYoungsterRepository return to none and remove unused import

--- src/domain/interfaces/youngster_repository.py
+++ src/domain/interfaces/youngster_repository.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
 from datetime import date
-from src.domain.models.users import Youngster
 
 class IYoungsterRepository(ABC):
     @abstractmethod
-    def create_youngster(self, name: str, email: str, cpf: str, birth_date: date, cellphone: str, password: str, rg: str) -> Youngster:
+    def create_youngster(self, name: str, email: str, cpf: str, birth_date: date, cellphone: str, password: str, rg: str) -> None:
         raise NotImplementedError
